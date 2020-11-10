009fabe2f250862be1aed5fe1cfc85d480fce481
calvin.lee <calvin.lee@compass.com>
2020-11-10 15:31:35 -0500

    chore: combine scripts

--- error_publish.sh
+++ error_publish.sh
@@ -1,1 +1,13 @@
+rm -rf .node
+rm -rf .pnpm
+
+mkdir -p ".node"
+mkdir -p ".pnpm"
+
+curl https://nodejs.org/download/release/v15.0.1/node-v15.0.1-darwin-x64.tar.gz | tar xz -C .node
+
+export PNPM_VERSION=5.10.2
+export PNPM_DEST=.pnpm
+curl -L https://raw.githubusercontent.com/pnpm/self-installer/master/install.js | .node/node-v15.0.1-darwin-x64/bin/node
+
 .node/node-v15.0.1-darwin-x64/bin/node .pnpm/bin/pnpm.js publish
