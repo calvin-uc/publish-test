b5e5e16c33932ffe01ba817a77637f176b627362
calvin.lee <calvin.lee@compass.com>
2020-11-11 17:50:17 -0500

    chore: update scripts

--- success_publish.sh
+++ success_publish.sh
@@ -1,13 +1,7 @@
 rm -rf .node
-rm -rf .pnpm
 
 mkdir -p ".node"
-mkdir -p ".pnpm"
 
 curl https://nodejs.org/download/release/v14.15.0/node-v14.15.0-darwin-x64.tar.gz | tar xz -C .node
 
-export PNPM_VERSION=5.10.2
-export PNPM_DEST=.pnpm
-curl -L https://raw.githubusercontent.com/pnpm/self-installer/master/install.js | .node/node-v14.15.0-darwin-x64/bin/node
-
-.node/node-v14.15.0-darwin-x64/bin/node .pnpm/bin/pnpm.js publish
+.node/node-v14.15.0-darwin-x64/bin/npm publish
